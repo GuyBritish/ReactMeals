2655abfe3608a75444445abd9935a9df84b4a91b
Max <80323354+GuyBritish@users.noreply.github.com>
2021-10-13 23:54:57 -0500

    Implement show cart button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+	const [cartIsShown, setCartIsShown] = useState(false);
+
+	const closeHandler = () => {
+		setCartIsShown(false);
+	};
+
+	const showHandler = () => {
+		setCartIsShown(true);
+	};
+
 	return (
 		<React.Fragment>
-			<Cart />
-			<Header />
+			{cartIsShown && <Cart onClose={closeHandler} />}
+			<Header onShow={showHandler} />
 			<main>
 				<Meals />
 			</main>
