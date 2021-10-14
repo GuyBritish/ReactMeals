dd5aa0cfc2bc2c21086ca17486188a2bd35e8b9e
Max <80323354+GuyBritish@users.noreply.github.com>
2021-10-13 23:24:08 -0500

    Add styling

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,14 @@
 import React from "react";
 
+import classes from "./Cart.module.css";
+
 const Cart = (props) => {
 	const cartItems = [{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
 		<li>{item.name}</li>
 	));
 	return (
 		<div>
-			<ul> {cartItems} </ul>
+			<ul className={classes["cart-items"]}> {cartItems} </ul>
 			<div></div>
 			<div></div>
 		</div>
