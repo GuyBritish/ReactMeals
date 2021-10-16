c0ad9ff4e8201456552efd0fdfadb9bf83540ef6
Max <80323354+GuyBritish@users.noreply.github.com>
2021-10-16 04:27:45 -0500

    add ADD action to cart reducer

--- src/store/cartPorvider,.js
+++ src/store/cartPorvider,.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 import CartContext from "./cartContext";
 
-const CartProvider = (props) => {
-	const addToCartHandler = (item) => {};
+const cartDefaultState = {
+	items: [],
+	total: 0,
+};
 
-	const removeFromCartHandler = (id) => {};
+const cartReducer = (state, action) => {
+	if (action.type === "ADD") {
+		const newCart = state.items.concat(action.item);
+		const newTotal = state.total + action.item.price * action.item.amount;
+		return {
+			items: newCart,
+			total: newTotal,
+		};
+	}
+	if (action.type === "RMV") {
+	}
+	return cartDefaultState;
+};
+
+const CartProvider = (props) => {
+	const [cartState, dispatchCartAction] = useReducer(cartReducer, cartDefaultState);
+
+	const addToCartHandler = (item) => {
+		dispatchCartAction({ type: "ADD", item: item });
+	};
+
+	const removeFromCartHandler = (id) => {
+		dispatchCartAction({ type: "RMV", id: id });
+	};
 
 	const cartContext = {
-		items: [],
-		total: 0,
+		items: cartState.items,
+		total: cartState.total,
 		addItem: addToCartHandler,
 		removeItem: removeFromCartHandler,
 	};
