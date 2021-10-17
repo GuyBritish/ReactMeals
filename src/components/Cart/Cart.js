c0954d443245fd694247f74d5559149d2aa66c05
Max <80323354+GuyBritish@users.noreply.github.com>
2021-10-16 22:59:37 -0500

    Display cart items from context

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,25 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import CartContext from "../../store/cartContext";
 
 import Modal from "../UI/Modal";
 
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
-	const cartItems = [{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-		<li>{item.name}</li>
-	));
+	const cartCtx = useContext(CartContext);
+
+	const cartItems = cartCtx.items.map((item) => <li>{item.name}</li>);
+
+	const total = `$${cartCtx.total.toFixed(2)}`;
+
 	return (
 		<Modal onClose={props.onClose}>
 			<ul className={classes["cart-items"]}> {cartItems} </ul>
 			<div className={classes.total}>
 				<span> Total Amount </span>
-				<span> $$.$$ </span>
+				<span> {total} </span>
 			</div>
 			<div className={classes.actions}>
 				<button className={classes["button-alt"]} onClick={props.onClose}>
 					Close
 				</button>
-				<button className={classes.button}> Order </button>
+				{cartCtx.items.length > 0 && <button className={classes.button}> Order </button>}
 			</div>
 		</Modal>
 	);
