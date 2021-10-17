64f6544a67c7466678c7973b850d8e591d686322
Max <80323354+GuyBritish@users.noreply.github.com>
2021-10-16 23:06:17 -0500

    use CartItem component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,13 +3,27 @@ import React, { useContext } from "react";
 import CartContext from "../../store/cartContext";
 
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
 	const cartCtx = useContext(CartContext);
 
-	const cartItems = cartCtx.items.map((item) => <li>{item.name}</li>);
+	const cartItemRemoveHandler = (id) => {};
+
+	const cartItemAddHandler = (item) => {};
+
+	const cartItems = cartCtx.items.map((item) => (
+		<CartItem
+			key={item.id}
+			name={item.name}
+			amount={item.amount}
+			price={item.price}
+			onRemove={cartItemRemoveHandler.bind(null, item.id)}
+			onAdd={cartItemAddHandler.bind(null, item)}
+		/>
+	));
 
 	const total = `$${cartCtx.total.toFixed(2)}`;
 
